d0422981761da85ea646e73cae0cda8c1910e9ab
maximgames02 <103461154+maximgames02@users.noreply.github.com>
2025-01-04 14:12:34 +0300

    Update MenuAnim.lua

--- MenuAnim.lua
+++ MenuAnim.lua
@@ -2,22 +2,23 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 function Main(animId)
-	local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-	local Humanoid = Character:WaitForChild("Humanoid")
-	local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+	for _, dummy in ipairs(workspace.MainMenuDummies:GetChildren()) do
+		local Humanoid = Character:WaitForChild("Humanoid")
+		local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 
-	local animation = Instance.new("Animation")
-	animation.AnimationId = `rbxassetid://{animId}`
-	local animationTrack = Humanoid:LoadAnimation(animation)
+		local animation = Instance.new("Animation")
+		animation.AnimationId = `rbxassetid://{animId}`
+		local animationTrack = Humanoid:LoadAnimation(animation)
 
-	local function stopAllAnimations()
-		for _, track in pairs(Humanoid:GetPlayingAnimationTracks()) do
-			print(track)
-			track:Stop()
+		local function stopAllAnimations()
+			for _, track in pairs(Humanoid:GetPlayingAnimationTracks()) do
+				print(track)
+				track:Stop()
+			end
 		end
+		stopAllAnimations()
+		animationTrack:Play()
 	end
-	stopAllAnimations()
-	animationTrack:Play()
 end
 
 return Main
